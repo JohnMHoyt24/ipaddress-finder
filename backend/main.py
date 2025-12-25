f525bc0dd16efb26eaad717d29562eda53128d79
JohnMHoyt24 <Jmhoyt6355@gmail.com>
2025-12-25 10:39:29 -0500

    Configure main.py to call external API. Imported main.py to __init__.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+"""
+This file makes a GET request to the Ipify API where the API key and the API URL are stored
+as environmental variables.
+"""
+
+from fastapi import FastAPI, HTTPException, Query
 import os
 from dotenv import load_dotenv
 import httpx
@@ -9,3 +14,20 @@ app = FastAPI
 
 api_key = os.getenv("IPIFY_API_KEY")
 api_url = os.getenv("IPIFY_URL")
+
+@app.get('/geolocation')
+def get_ip_location(ip: str = Query(..., example='8.8.8.8')):
+    params = {
+        "api_key": api_key,
+        "ip_address": ip
+    }
+
+    try:
+        response = httpx.get(api_url, params=params)
+
+        response.raise_for_status()
+
+        return response.json()
+
+    except httpx._exceptions.HTTPError as http_err:
+        raise HTTPException(status_code=response.status_code, detail=f"API Error: {http_err}")
