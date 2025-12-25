90f1b3ae2f953b7a229fd71b2d5d573a0b05ad86
JohnMHoyt24 <Jmhoyt6355@gmail.com>
2025-12-25 16:22:00 -0500

    Set app = FastAPI(). Updated function to handle API calls asynchronously. Updated exception handling to throw 500 errors.

--- backend/main.py
+++ backend/main.py
@@ -10,24 +10,25 @@ import httpx
 
 load_dotenv()
 
-app = FastAPI
+app = FastAPI()
 
 api_key = os.getenv("IPIFY_API_KEY")
 api_url = os.getenv("IPIFY_URL")
 
 @app.get('/geolocation')
-def get_ip_location(ip: str = Query(..., example='8.8.8.8')):
+async def get_ip_location(ip: str = Query(..., example='8.8.8.8')):
     params = {
         "api_key": api_key,
         "ip_address": ip
     }
 
     try:
-        response = httpx.get(api_url, params=params)
+        async with httpx.AsyncClient() as client:
+            response = await client.get(api_url, params=params)
+            response.raise_for_status()
+            return response.json()
 
-        response.raise_for_status()
-
-        return response.json()
-
-    except httpx._exceptions.HTTPError as http_err:
-        raise HTTPException(status_code=response.status_code, detail=f"API Error: {http_err}")
+    except httpx.HTTPStatusError as http_err:
+        raise HTTPException(status_code=http_err.response.status_code, detail=f"API Error: {http_err}")
+    except httpx.RequestError as req_err:
+        raise HTTPException(status_code=500, detail=f"Request Error: {req_err}")
