a898a1da5bb5ce13ca4d423e1a3c91616bb04621
JohnMHoyt24 <Jmhoyt6355@gmail.com>
2026-01-09 11:59:25 -0500

    Imported useState and useEffect. Created hooks for ip address and location.

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react';
+
+const [ipAddress, setIpAddress] = useState('');
+const [location, setLocation] = useState('');
+
+interface SearchIpAddress{
+    ipAddress: string;
+    location: string;
+}
+
+const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!ipAddress){
+        alert('Please enter an IP address or domain');
+    }
+}
+
 const SearchBar: React.FC = () => {
     return(
-        <div>
-            <input type="search" />
+        <div className="bg-blue-300 justify-center items-center h-25">
+            <h1 className="text-2xl">IP Address Tracker</h1>
+            <input type="search" placeholder="Search for any IP address or domain" className="border-black
+            bg-white w-1/2 justify-center items-center" />
         </div>
     );
 }
