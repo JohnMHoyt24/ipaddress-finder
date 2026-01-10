9bfd268fc1e3b44bd230f9de9039f0d28a41d2c8
JohnMHoyt24 <Jmhoyt6355@gmail.com>
2026-01-09 20:21:01 -0500

    Added useEffect hook to API.

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,17 +1,73 @@
 import { useState, useEffect } from 'react';
+import IPPanel from './IPPanel';
+import Map from './Map';
 
+const [search, setSearch] = useState('');
 const [ipAddress, setIpAddress] = useState('');
 const [location, setLocation] = useState('');
+const [timezone, setTimezone] = useState('');
+const [isp, setIsp] = useState('');
+const [loading, setLoading] = useState(false);
+const [errors, setErrors] = useState<{[key: string]: string}>({});
 
 interface SearchIpAddress{
-    ipAddress: string;
-    location: string;
+    ip: string;
+    location: {
+        country: string;
+        region: string;
+        city: string;
+        lat: number;
+        lng: number;
+        postalCode: string;
+        timezone: string;
+        geonameId: number;
+    };
+    as: {
+        asn: number;
+        name: string;
+        route: string;
+        domain: string;
+        type: string;
+    };
+    isp: string;
 }
+
+const REGEX_IP_ADDRESS = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 
 const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!ipAddress){
-        alert('Please enter an IP address or domain');
+    if (!REGEX_IP_ADDRESS.test(search)){
+        setErrors({search: 'Invalid IP address or domain'});
+    }
+    else if(!search){
+        setErrors({search: 'Please enter an IP address or domain'});
+    }
+    else {
+        setErrors({});
+
+        useEffect(() => {
+            const fetchData = async() => {
+                try {
+                    const response = await fetch('http://127.0.0.1:8000/geolocation');
+
+                    if(!response.ok){
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+
+                    const result = await response.json();
+                    setIpAddress(result.ip);
+                    setErrors({});
+                }
+                catch (err: any){
+                    setErrors(err.message);
+                }
+                finally{
+                    setLoading(false);
+                }
+            }
+
+            fetchData();
+        }, []);
     }
 }
 
